fce1d425566ebf6137e46a0df8e9939c5f49b666
Norbert Gajda <norbert.gajda@gmail.com>
2020-09-13 12:59:04 +0200

    add old files from other folders and then create witter folder

--- cargo_first/helo_world/src/main.rs
+++ cargo_first/helo_world/src/main.rs
@@ -1,7 +1,9 @@
 // mod pointer_ref;
-mod structures;
+// mod structures;
+mod vect_hash_maps;
 
 fn main() {
 	// pointer_ref::run();
-	structures::run();
+	// structures::run();
+	vect_hash_maps::run();
 }
