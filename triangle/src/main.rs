3adcfd67a38a470c208a80f896aceac27540423f
Norbert Gajda <norbert.gajda@gmail.com>
2020-10-25 23:53:52 +0100

    finish sierpiński triangle

--- triangle/src/main.rs
+++ triangle/src/main.rs
@@ -5,17 +5,18 @@ extern crate image;
 extern crate rand;
 
 use rand::Rng;
-use std::fs::File;
+// use std::fs::File;
 use std::path::Path;
+// use image::ColorType;
 
-/// points used to build triangle
+/// points used to build triangle and plot in the canvas
 pub struct Point {
     x: u32,
     y: u32
 }
 
-const WIDTH: u32 = 800;
-const HEIGHT: u32 = 600;
+const WIDTH: u32 = 1800;
+const HEIGHT: u32 = 1600;
 
 
 
@@ -29,16 +30,19 @@ fn main() {
             image::Luma([255u8])
         }
     });
-    let mut cnt: u32 = 10_000;
+    // counter
+    let mut cnt: u32 = 1000_000;
 
+    // fixed sized array, not a vector
     let pts: [Point; 3] = [
         Point{x: WIDTH/2, y: 0},
-        Point{x: 0, y: HEIGHT/2},
+        Point{x: 0, y: HEIGHT},
         Point{x: WIDTH, y: HEIGHT}
     ];
 
     let mut num: usize;
 
+    // arbitrary point
     let mut p = Point{x: 350, y: 350};
 
     let pixel = img[(0,0)];
@@ -51,8 +55,8 @@ fn main() {
         img.put_pixel(p.x, p.y, pixel);
     }
 
-    let ref mut fout = File::create(&Path::new("tri.png")).unwrap();
+    // let ref mut fout = File::create(&Path::new("tri.png")).unwrap();
 
-    let _ = image::ImageEncoder(img).;
+    img.save(Path::new("test_tri.png")).unwrap();
 
 }
