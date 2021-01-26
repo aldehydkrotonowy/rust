0ba8c766c29e06ed75008232f20f4dd6344c56d7
Norbert Gajda <norbert.gajda@gmail.com>
2021-01-26 13:06:45 +0100

    node

---
+++ primes/src/tree.rs
@@ -0,0 +1,16 @@
+mod Tree {
+
+
+  struct Node {
+    data:i32;
+    left: Node;
+    right: Node;
+  }
+
+  pub fn testCreateNode(){
+    let singleNode: Node = Node {data: 2, left: null, right: null}
+  }
+}
+
+
+
